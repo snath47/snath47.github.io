50a030c9838dd8b26fe1a7d6ebafde06885642c1
snath47 <71223795+snath47@users.noreply.github.com>
2026-05-27 18:20:36 +0000

    Deploying to gh-pages from @ snath47/snath47.github.io@988709009aabc32dc92fe91f7126f995665e5746 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,7 +497,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%61%6E%64%69%70%61%6E.%6E%61%74%68@%66%61%75.%64%65", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -519,13 +519,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',

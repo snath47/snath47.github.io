028c14337ead93578a58b2d53cdc0ea63b40f32f
snath47 <71223795+snath47@users.noreply.github.com>
2026-05-28 18:00:46 +0000

    Deploying to gh-pages from @ snath47/snath47.github.io@0900e9c18fd481684373112d771484c4f38dcc52 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,12 +414,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "books-dynamical-systems-in-neuroscience",
+      },{id: "books-dynamical-systems-in-neuroscience",
           title: 'Dynamical Systems in Neuroscience',
           description: "",
           section: "Books",handler: () => {

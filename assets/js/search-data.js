811893ea045692f25d8813b42420c5f3f151e3e5
snath47 <71223795+snath47@users.noreply.github.com>
2026-05-28 19:08:18 +0000

    Deploying to gh-pages from @ snath47/snath47.github.io@54ad25dd14bc746f6b650aa27b84953edd75d715 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Data Science MSc candidate at FAU with expertise in ML, dynamical systems, and HPC.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -493,7 +493,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/resume_academic.pdf", "_blank");
         },
       },{
         id: 'social-email',
@@ -503,11 +503,11 @@ ninja.data = [{
           window.open("mailto:%73%61%6E%64%69%70%61%6E.%6E%61%74%68@%66%61%75.%64%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/snath47", "_blank");
         },
       },{
         id: 'social-rss',
@@ -515,13 +515,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-linkedin',

57b6921a5259a5800ad2b8373a091f77896ed4f4
snath47 <71223795+snath47@users.noreply.github.com>
2026-07-23 13:46:42 +0000

    Deploying to gh-pages from @ snath47/snath47.github.io@0c4d7b750026560f1c9710158eb7a34fad9a9d5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,21 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
+  },{id: "nav-repositories",
           title: "repositories",
           description: "Selected research and coursework repositories",
           section: "Navigation",
